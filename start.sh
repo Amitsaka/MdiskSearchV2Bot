d63044be5069aedc498630e9b9ddfc418cfd9bb9
Amitsaka <119963844+Amitsaka@users.noreply.github.com>
2022-12-24 01:11:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Amitsaka/MdiskSearchV2Bot.git /MdiskSearchV2Bot
+cd /MdiskSearchV2Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
